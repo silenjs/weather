dc1236a6762e57698fe2a2c9514db3d253b70d73
silen <silenjs@gmail.com>
2015-06-17 16:53:03 +0800

    show future 24hours

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,25 @@
 var iconMap = (function(){
     var map={
         // 500 小雨 501 中雨 502 大雨
-        '500':'rain_s',
-        '501':'rain_m',
-        '502':'rain_h',
-        '800':'sun',
-        '801':'sun_cloudy',
-        '802':'cloudy',
-        '803':'cloudy',
-        '804':'day_cloudy',
-        //Object {id: 800, main: "Clear", description: "晴", icon: "01d"}
-        '01d':'sun',
-        //Object {id: 800, main: "Clear", description: "晴", icon: "02d"}
-        '02d':'sun',
-        //{id: 802, main: "Clouds", description: "多云", icon: "03n"}
-        '03n':'cloudy',
-        '04n':'cloudy',
-        //Object {id: 802, main: "Clouds", description: "多云", icon: "03d"}
-        '03d':'cloudy',
-        '04d':'day_cloudy',
+        '500d':'rain_s',
+        '501d':'rain_m',
+        '502d':'rain_h',
+        '500n':'night_rain',
+        '501n':'night_rain',
+        '502n':'night_rain',
+        '800d':'sun',
+        '801d':'sun_cloudy',
+        '802d':'cloudy',
+        '803d':'cloudy',
+        '804d':'sun_cloudy',
+        '800n':'night',
+        '801n':'night_cloudy',
+        '802n':'night_cloudy',
+        '803n':'night_cloudy',
+        '804n':'night_cloudy',
     }
-    return function(code){
-        return map[code]||'cloudy';
+    return function(code,type){
+        return map[code+type.slice(-1)]||(type.slice(-1)=='n'?'night':'sun');
     }
 })();
 var dayMap = (function () {
@@ -31,21 +29,15 @@ var dayMap = (function () {
     };
 })();
 
-//   .ico_sun:before {content: "\e600";}
-//    .ico_cloudy:before {content: "\e609";}
-//    .ico_day_cloudy:before {content: "\e605";}
-//    .ico_night_cloudy:before {content: "\e602";}
-//    .ico_rain_s:before {content: "\e601";}
-//    .ico_rain_m:before {content: "\e604";}
-//    .ico_rain_h:before {content: "\e603";}
-//    .ico_thunder:before {content: "\e606";}
-//    .ico_thunder_rain:before {content: "\e607";}
-//    .ico_thunder_rain_h:before {content: "\e608";}
+function formatHour(hour){
+    return hour>10?hour:'0'+hour;
+}
 
 +function init(){
     if(navigator.standalone||!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)){
+        document.querySelector('.loading').style.display="";
         var script = document.createElement('script');
-        script.src = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Shenzhen,CN&cnt=7&mode=json&lang=zh_cn&callback=parse";
+        script.src = "http://api.openweathermap.org/data/2.5/forecast?q=Shenzhen,CN&mode=json&lang=zh_cn&callback=parse";
         document.body.appendChild(script);
     }else{
         document.querySelector('.adddesk').style.display="";
@@ -54,46 +46,49 @@ var dayMap = (function () {
 }();
 
 function parse(data){
-    var list = data.list,dataArr=[];
-    list.forEach(function(item){
-        dataArr.push({
-            'date':item.dt*1000,
-            'label':dayMap(new Date(item.dt*1000).getDay()),
-            'temp':item.temp,
-            'icon':item.weather[0].id,
-            'main':item.weather[0].main,
-            'desc':item.weather[0].description
-        })
+    var current = data.list.filter(function(item){
+        return Date.now()>=item.dt*1000&&Date.now()-item.dt*1000<1000*60*60*3;
+    })[0];
+    var hours = data.list.filter(function(item){
+        return item.dt*1000>Date.now()&&item.dt*1000-Date.now()<1000*60*60*3*7;
     })
-    render(dataArr);
+    renderToday(current,hours);
+    var script = document.createElement('script');
+    script.src="http://api.openweathermap.org/data/2.5/forecast/daily?q=Shenzhen,CN&cnt=7&mode=json&lang=zh_cn&callback=renderFuture";
+    document.body.appendChild(script);
     setTimeout(function(){
         document.querySelector('.loading').classList.add('loadover');
     },2000)
 }
 
-function render(data){
-    renderToday(data[0]);
-    renderFuture(data);
-}
+function renderToday(current,hours){
+    console.log(arguments);
+    var dateStr=new Date().toUTCString().split(' ').slice(0,3).join(' '),
+        descStr=current.weather[0].description,
+        tempStr=(current.main.temp-273.15).toFixed(1);
+    var hoursStr = (function(hours){
+        var str = [];
+        hours.forEach(function(item){
+            str.push('<dl><dt>'+formatHour(new Date(item.dt*1000).getHours())+'</dt><dd><i class="ico ico_'+iconMap(item.weather[0].id,item.weather[0].icon)+'"></i><em>'+Math.round(item.main.temp-273.15)+'℃</em></dd></dl>')
+        })
+        return str;
+    })(hours).join(' ');
+    document.querySelector('.today').innerHTML = 'Shenzhen '+dateStr+'<strong>'+ descStr +' '+ tempStr +'℃</strong>';
+    document.querySelector('.today_weather').innerHTML = '<i class="ico ico_'+iconMap(current.weather[0].id,current.weather[0].icon)+'"></i>';
+    document.querySelector('.future24').innerHTML=hoursStr;
 
-function renderToday(today){
-    var dateStr=new Date(today.date).toUTCString().split(' ').slice(0,3).join(' '),
-        descStr=today.desc,
-        tempStr=(today.temp.day-273.15).toFixed(1);
-    document.querySelector('#today').innerHTML = 'Shenzhen '+dateStr+'<strong>'+ descStr +' '+ tempStr +'℃</strong>';
-    document.querySelector('#today_weather').innerHTML = '<i class="ico ico_'+iconMap(today.icon)+'"></i>';
 }
 
 function renderFuture(data){
     console.log(data);
     var fHtml = '';
-    data.forEach(function(item){
+    data.list.forEach(function(item){
         fHtml += '<dl>';
-        fHtml += '<dt>'+item.label+'</dt>';
+        fHtml += '<dt>'+dayMap(new Date(item.dt*1000).getDay())+'</dt>';
         fHtml += '<dd>'
-        fHtml += '<i class="ico ico_'+iconMap(item.icon)+'"></i>';
+        fHtml += '<i class="ico ico_'+iconMap(item.weather[0].id,item.weather[0].icon)+'"></i>';
         fHtml += '<em>'+Math.round(item.temp.day-273.15)+'℃</em>';
-        fHtml += '<span><em>'+Math.round(item.temp.night-273.15)+'℃</em><i class="ico ico_'+iconMap(item.icon)+'"></i><br/>'+(new Date(item.date).getMonth()+1)+'/'+new Date(item.date).getDate()+'</span>'
+        fHtml += '<span>'+(new Date(item.dt*1000).getMonth()+1)+'/'+new Date(item.dt*1000).getDate()+'</span>'
         fHtml += '</dd>';
         fHtml += '</dl>';
     })
