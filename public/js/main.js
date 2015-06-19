8d3bef7fe1500831b54fc0bf90a61601dc10faef
silen <silenjs@gmail.com>
2015-06-19 17:21:46 +0800

    location

--- public/js/main.js
+++ public/js/main.js
@@ -33,6 +33,70 @@ function formatHour(hour){
     return hour>10?hour:'0'+hour;
 }
 
++function() {
+    var map = document.createElement('script');
+    map.src = 'http://webapi.amap.com/maps?v=1.3&key=d06e335a59eabcc27ae1028844b5b8c8&callback=initMap';
+    document.body.appendChild(map);
+}();
+
+var suixMap = null;
+var initMap = function(){
+    suixMap = new AMap.Map('mapContainer',{resizeEnable: true});
+    getLngLat()
+}
+
+function getLngLat(){
+    var geolocation;
+    suixMap.plugin('AMap.Geolocation',function(){
+        geolocation = new AMap.Geolocation({
+            enableHighAccuracy: true,//是否使用高精度定位，默认:true
+            timeout: 10000,          //超过10秒后停止定位，默认：无穷大
+            maximumAge: 0,           //定位结果缓存0毫秒，默认：0
+            convert: true,           //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+            showButton: true,        //显示定位按钮，默认：true
+            buttonPosition: 'LB',    //定位按钮停靠位置，默认：'LB'，左下角
+            buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+            showMarker: true,        //定位成功后在定位到的位置显示点标记，默认：true
+            showCircle: true,        //定位成功后用圆圈表示定位精度范围，默认：true
+            panToLocation: true,     //定位成功后将定位到的位置作为地图中心点，默认：true
+            zoomToAccuracy:true      //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+        });
+        suixMap.addControl(geolocation);
+        AMap.event.addListener(geolocation, 'complete', geoSuccess);//返回定位信息
+        AMap.event.addListener(geolocation, 'error', geoErr);      //返回定位出错信息
+    })
+    geolocation.getCurrentPosition();
+}
+
+function geoSuccess(pos){
+    console.log(pos);
+    var lnglatXY = new AMap.LngLat(pos.position.lng,pos.position.lat);
+    AMap.service(["AMap.Geocoder"], function() {
+        MGeocoder = new AMap.Geocoder({
+            radius: 1000,
+            extensions: "all"
+        });
+        //逆地理编码
+        MGeocoder.getAddress(lnglatXY, function(status, result){
+            if(status === 'complete' && result.info === 'OK'){
+                formatAddr(result)
+            }
+        });
+    });
+
+}
+function geoErr(err){
+    console.log(err);
+}
+
+function formatAddr(res){
+    console.log(res);
+    alert(res.regeocode.formattedAddress);
+    alert(res.regeocode.addressComponent.province);
+    alert(res.regeocode.addressComponent.city);
+    alert(res.regeocode.addressComponent.district);
+}
+
 +function init(){
     if(navigator.standalone||!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)){
         window.slogan&&(document.querySelector('.oneday').innerHTML=slogan[Math.floor(Math.random()*slogan.length)]);
